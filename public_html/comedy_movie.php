7ab580789f8a89c216feba9695a2ebee520ae067
BrysonCook <35880743+BrysonCook@users.noreply.github.com>
2018-11-01 00:11:14 -0500

    Add files via upload

---
+++ public_html/comedy_movie.php
@@ -0,0 +1,199 @@
+<!-- This page is created by Bishnu Adhikari-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="css_test2.css">
+
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.3/sandstone/bootstrap.min.css">
+    <br>
+    <br>
+    <br>
+    <br>
+    <title>Comedy Movie</title>
+
+    <style>
+        .navbar {
+            background-color: #333; /* Black background color */
+            position: fixed; /* Make it stick/fixed */
+            top: 0px; /* Hide the navbar 50 px outside of the top view */
+            width: 100%; /* Full width */
+            transition: top 0.3s; /* Transition effect when sliding down (and up) */
+        }
+        /* Style the navbar links */
+        .navbar a {
+            float: left;
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 15px;
+            text-decoration: none;
+        }
+        .navbar a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+    </style>
+</head>
+
+<body>
+<?php include '../resources/navigation.php'; ?>
+
+<section>
+<header>
+    <h1>Comedy Movies</h1>
+</header>
+<br><br>
+
+<h2>Tide Pod Man</h2>
+<br>
+<img src="img/comedy_movie.png" alt="Tide Pod Man" />
+<br><br>
+<div class="boxed">
+<article class="article">
+    <div class="row">
+        <div class="mycolor col-12 col-sm-6">
+
+    <h3><b>Description</b></h3>
+
+<p>Paul Nerf (Ryan Reynolds) was just an ordinary guy... until he swallowed a Tide Pod. Discovering endless powers and abilities, Paul is being targeted by the worlds scariest assassinsand can only be saved by eating more Tide Pods to strengthen his abilites. The problem is, with every Pod he eats, the weaker his immune system becomes. Will Paul find the cure to his Tide Pod powers? or will he have to become the most unstoppable man alive at a life threatening level?</p>
+    </div>
+    <div class="mycolor col-12 col-sm-6">
+<h2><b>More Info</b></h2>
+
+<p>Director&colon; Adam Sandler
+Cast&colon; Ryan Reynolds, Samuel L. Jackson, and Melissa McCarthy.
+PG-13
+3D Showing at 5&colon;10</p>
+</div>
+
+<div class="p-3 mb-2 bg-warning text-dark">We are extremely sorry if this movie isn't worth watching.</div>
+</div>
+<div class="mycolor col-12 col-sm-6">
+
+</article>
+</section>
+<div class="boxed">
+<h2>Showtimes</h2>
+<table class="table table-striped">
+    <thead>
+        <tr>
+        
+            <th scope="col">Monday</th>
+
+            <th scope="col">Tuesday</th>
+
+            <th scope="col">Wednesday</th>
+
+            <th scope="col">Thursday</th>
+
+            <th scope="col">Friday</th>
+
+            <th scope="col">Saturday</th>
+
+            <th scope="col">Sunday</th>
+         </tr>
+     </thead>
+
+     <tbody>
+        <tr>
+            <td>1&colon;00</td>
+
+            <td>1&colon;00</td>
+
+            <td>1&colon;00</td>
+
+            <td>1&colon;00</td>
+
+            <td>1&colon;00</td>
+
+            <td>1&colon;00</td>
+
+            <td>1&colon;00</td>
+        </tr>
+
+        <tr>
+            <td>2&colon;30</td>
+
+            <td>2&colon;30</td>
+
+            <td>2&colon;30</td>
+
+            <td>2&colon;30</td>
+
+            <td>2&colon;30</td>
+
+            <td>2&colon;30</td>
+
+            <td>2&colon;30</td>
+        </tr>
+
+        <tr>
+            <td>5&colon;10</td>
+
+            <td>5&colon;10</td>
+
+            <td>5&colon;10</td>
+
+            <td>5&colon;10</td>
+
+            <td>5&colon;10</td>
+
+            <td>5&colon;10</td>
+
+            <td>5&colon;10</td>
+        </tr>
+
+        <tr>
+            <td>7&colon;25</td>
+
+            <td>7&colon;25</td>
+
+            <td>7&colon;25</td>
+
+            <td>7&colon;25</td>
+
+            <td>7&colon;25</td>
+
+            <td>7&colon;25</td>
+
+            <td>7&colon;25</td>
+        </tr>
+
+        <tr>
+            <td>9&colon;40</td>
+
+            <td>9&colon;40</td>
+
+            <td>9&colon;40</td>
+
+            <td>9&colon;40</td>
+
+            <td>9&colon;40</td>
+
+            <td>9&colon;40</td>
+
+            <td>9&colon;40</td>
+        </tr>
+    </tbody>
+</table>
+</div>
+</div>
+
+     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<br>
+
+<?php include '../resources/footer.php'; ?>
+
+</body>
+
+</html>
