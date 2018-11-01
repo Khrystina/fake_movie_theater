1d36bbcd46c13b50aaec76fa715e780d6b0591dc
BrysonCook <35880743+BrysonCook@users.noreply.github.com>
2018-11-01 00:03:52 -0500

    Add files via upload

---
+++ resources/footer.php
@@ -0,0 +1,3 @@
+<footer>
+  &copy; 2018 by Bryson Cook
+</footer>
---
+++ resources/navigation.php
@@ -0,0 +1,51 @@
+ <h1>Simpson Theater</h1>
+ <style> 
+	p{
+		color: black;
+	}
+        .navbar {
+            background-color: #333; /* Black background color */
+            position: fixed; /* Make it stick/fixed */
+            top: 0px; /* Hide the navbar 50 px outside of the top view */
+            width: 100%; /* Full width */
+            transition: top 0.3s; /* Transition effect when sliding down (and up) */
+        }
+        /* Style the navbar links */
+        .navbar a {
+            float: left;
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 15px;
+            text-decoration: none;
+        }
+        .navbar a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+</style>
+ <ul class="nav navbar-nav">
+ <li>
+        <a class="nav-link" href="index.php">About page</a>
+    </li>
+
+    <li>
+        <a class="nav-link" href="space_ghosts.php">Horror Movies</a>
+    </li>
+
+    <li>
+        <a class="nav-link" href="chef_dog.php">Fantasy Movies</a>
+    </li>
+
+    <li>
+        <a class="nav-link" href="comedy_movie.php">Comedy Movies</a>
+    </li>
+    
+    <li>
+        <a class="nav-link" href="action_movie.php">Action Movies</a>
+    </li>
+
+    <li>
+        <a class="nav-link" href="love_birds.php">Romantic Movies</a>
+    </li>
+</ul>
